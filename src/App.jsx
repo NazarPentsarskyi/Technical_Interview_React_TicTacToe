878a792a6c39e6a1726b2b7a587cfac6957e00ec
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2023-10-01 00:11:56 +0300

    started changed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Matrix from './components/Matrix';
+import tictactoe from '../public/tictactoe_white.svg';
 import './App.css';
 
 function App() {
   return (
     <>
+    <div className='header'>
       <h1>Tic Tac Toe</h1>
+      <img className='logo' src={tictactoe} alt='logo tic tac toe' />
+    </div>
       <Matrix />
     </>
   )
