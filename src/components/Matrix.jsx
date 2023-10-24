eb2f42b8058c4b65bd92818d0780024f59c7b21c
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2023-10-24 23:36:23 +0300

    changed ways to logos

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
-import tictactoeWhite from '../../public/tictactoe_white.svg';
-import tictactoeBlue from '../../public/tictactoe_blue.svg';
+import tictactoeWhite from '../assets/tictactoe_white.svg';
+import tictactoeBlue from '../assets/tictactoe_blue.svg';
 import Board from './Board';
 import Status from './Status';
 
@@ -48,17 +48,14 @@ const Matrix = () => {
   const checkWinner = useCallback(() => {
 
     const winningCombinations = [
-      //Rows
       { combo: [0, 1, 2], strikeClass: 'strike-row-1' },
       { combo: [3, 4, 5], strikeClass: 'strike-row-2' },
       { combo: [6, 7, 8], strikeClass: 'strike-row-3' },
     
-      //Columns
       { combo: [0, 3, 6], strikeClass: 'strike-column-1' },
       { combo: [1, 4, 7], strikeClass: 'strike-column-2' },
       { combo: [2, 5, 8], strikeClass: 'strike-column-3' },
     
-      //Diagonals
       { combo: [0, 4, 8], strikeClass: 'strike-diagonal-1' },
       { combo: [2, 4, 6], strikeClass: 'strike-diagonal-2' },
     ];
